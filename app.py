efec344111d151f050bd2c3ac779f76faee8953e
Jameel785 <jameelruhomaun@hotmail.com>
2023-02-25 21:02:20 +0000

    Creating the basic routes for the platform

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template, request
 
 # this will convert the file into a web application
 app = Flask(__name__)
+
+# users can access the home page using the home path or default path
+@app.route("/home")
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html", title="home")
+
+# Access the login page with the login route
+@app.route("/login")
+def login():
+    return render_template("login.html", title="login")
+
+# Access the register page with the register route
+@app.route("/register")
+def register():
+    return render_template("register.html", title="register")
