bf0a6de95b87a1e4f3b5805c4231ce882b2cd89d
Jameel785 <jameelruhomaun@hotmail.com>
2023-02-26 17:13:13 +0000

    Creating the nav bar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 # this will convert the file into a web application
 app = Flask(__name__)
