59d03269961f0d2eb3e38ad7a4ca09dee829ee0e
Jameel785 <jameelruhomaun@hotmail.com>
2023-03-06 09:58:02 +0000

    Creating the registration form

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 # this will convert the file into a web application
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '15fe26a73acd9ed948d11ca07f2b50aa'
 
 # users can access the home page using the home path or default path
 @app.route("/home")
@@ -10,11 +13,22 @@ def home():
     return render_template("home.html", title="home")
 
 # Access the login page with the login route
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", title="login")
+    form = LoginForm()
+    return render_template("login.html", title="login", form=form)
 
 # Access the register page with the register route
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html", title="register")
+    # Creates an instance of the registration form
+    form = RegistrationForm()
+
+    # If the form has been submitted and is valid,
+    # Flash an account created success message and redirect the user to the home page
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
+
+    # Render the register template with the form instance
+    return render_template("register.html", title="register", form=form)
--- forms.py
+++ forms.py
@@ -1,44 +1,84 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField
-from wtforms.validators import ValidationError, Optional
+from wtforms import StringField, PasswordField, SelectField, SubmitField
+from wtforms.validators import ValidationError
 
+#define RegistrationForm class
 class RegistrationForm(FlaskForm):
+    # Check if field data exists
     def data_required(self, field):
         if not field.data:
-            raise ValidationError('This field is required')
+            raise ValidationError('[This field is required]')
 
+    # Check if username length is within range
     def validate_username_length(self, field):
-        min_length = 6
+        min_length = 4
         max_length = 20
         if len(field.data) < min_length or len(field.data) > max_length:
-            raise ValidationError(f'Username must be between {min_length} and {max_length} characters long')
+            raise ValidationError(f'[Username must be between {min_length} and {max_length} characters long]')
 
+    # Check if email is valid school email
     def validate_lydiard_email(self, field):
         if '@lydiardparkacademy.org.uk' not in field.data:
-            raise ValidationError('Invalid email, please use your school email')
+            raise ValidationError('[Invalid email, please use your school email]')
 
+    # Check if password and confirm_password fields match
     def validate_passwords_match(self, field):
         if self.password.data != field.data:
-            raise ValidationError('Passwords must match')
+            raise ValidationError('[Passwords must match]')
 
+    # Check if teacher registration key is valid
     def validate_teacher_reg_key(self, field):
         if self.role.data == 'teacher':
-            if field.data != 'TCHR786':
-                raise ValidationError('Invalid teacher registration key.')
-        elif self.role.data != 'teacher' and field.data:
-            raise ValidationError('This field is only applicable to teachers')
+            # Check if the field is empty
+            if not field.data:
+                # If the error message for empty field is not already present, append it to the error list
+                if '[Please enter your teacher registration key]' not in field.errors:
+                    field.errors.append('[Please enter your teacher registration key]')
+            # Check if the registration key entered is incorrect
+            elif field.data != 'TCHR786':
+                # If the error message for invalid key is not already present, append it to the error list
+                if '[Invalid teacher registration key]' not in field.errors:
+                    field.errors.append('[Invalid teacher registration key]')
+        else:
+            # Teacher role is not selected, so registration key is not required
+            pass
 
+    # Check if moderator registration key is valid
     def validate_moderator_reg_key(self, field):
         if self.role.data == 'moderator':
-            if field.data != 'MDRTR987':
-                raise ValidationError('Invalid moderator registration key.')
-        elif self.role.data != 'teacher' and field.data:
-            raise ValidationError('This field is only applicable to moderators')
+            # Check if the field is empty
+            if not field.data:
+                # If the error message for empty field is not already present, append it to the error list
+                if '[Please enter your moderator registration key]' not in field.errors:
+                    field.errors.append('[Please enter your moderator registration key]')
+            # Check if the registration key entered is incorrect
+            elif field.data != 'MDRTR987':
+                # If the error message for invalid key is not already present, append it to the error list
+                if '[Invalid moderator registration key]' not in field.errors:
+                    field.errors.append('[Invalid moderator registration key]')
+        else:
+            # Moderator role is not selected, so registration key is not required
+            pass
 
+    # Define form fields with validators
     username = StringField('Username', validators=[data_required, validate_username_length])
     email = StringField('Email', validators=[data_required, validate_lydiard_email])
     password = PasswordField('Password', validators=[data_required])
     confirm_password = PasswordField('Confirm Password', validators=[data_required, validate_passwords_match])
     role = SelectField('Sign Up As', choices=[('student', 'Student'), ('teacher', 'Teacher'), ('moderator', 'Moderator')], validators=[data_required])
-    teacher_reg_key = StringField('Teacher Registration Key', validators=[validate_teacher_reg_key, data_required, Optional()])
-    moderator_reg_key = StringField('Moderator Registration Key', validators=[validate_moderator_reg_key, data_required, Optional()])
+    teacher_reg_key = StringField('Teacher Registration Key', validators=[validate_teacher_reg_key])
+    moderator_reg_key = StringField('Moderator Registration Key', validators=[validate_moderator_reg_key])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    def data_required(self, field):
+        if not field.data:
+            raise ValidationError('This field is required')
+
+    def validate_lydiard_email(self, field):
+        if '@lydiardparkacademy.org.uk' not in field.data:
+            raise ValidationError('Invalid email, please use your school email')
+
+    email = StringField('Email', validators=[data_required, validate_lydiard_email])
+    password = PasswordField('Password', validators=[data_required])
+    submit = SubmitField('Login')
